11932f4258f884e74ceb09ec1006430f927937b1
EC2 Default User <ec2-user@ip-172-31-33-1.ap-northeast-2.compute.internal>
2024-01-09 14:04:47 +0000

    chore: user 정보 schema 수정

--- routes/auth/register.js
+++ routes/auth/register.js
@@ -4,11 +4,13 @@ var router = express.Router();
 var userdata = require('../../modules/userdata.js')
 
 router.post('/', async function(req, res, next) {
-	const { username, password, email } = req.body;
+	const { username, password, email, phone, birthday, gender } = req.body;
 	
-	const result = await userdata.createUser(username, password, email)
+	const result = await userdata.createUser(username, password, email, phone, birthday, gender);
 	
-	if (result instanceof Error) return res.status(401).send('The username or email is already exists.');
+	// TODO: 유일성을 해치는 인자가 무엇인지 result에 드러나도록 반환하게 할 것
+	// if (result instanceof Error) return res.status(401).send('The username or email is already exists.');
+	if (result instanceof Error) return res.status(401).send(result);
 	return res.status(200).send('Account successfully created.');
 	
 });
